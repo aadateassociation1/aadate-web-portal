437ff112a6e698afd52006192b8eb51d2728af1d
tejasprojects2026 <tejasprojects2026@users.noreply.github.com>
2026-09-20 18:42:17 +0530

    Show lower third in board video banner

--- src/routes/chairman.tsx
+++ src/routes/chairman.tsx
@@ -127,7 +127,7 @@ function Chairman() {
     <SiteLayout>
       <section className="relative overflow-hidden bg-black">
         <video
-          className="block aspect-video w-full object-cover object-[center_45%]"
+          className="block h-[52vw] min-h-[420px] max-h-[720px] w-full object-cover object-[center_62%]"
           src={BOARD_DIRECTORS_BANNER_VIDEO}
           autoPlay
           loop
